692fc98f72bb4cc6d082d17aead0e1dd2726b970
hseysen <hesenisgenderli999@gmail.com>
2024-03-23 17:34:57 +0400

    Utility function to compute mean and standard deviation of images

--- utils.py
+++ utils.py
@@ -1,9 +1,34 @@
-from torchmetrics.classification import MulticlassF1Score
+from operator import add
+from PIL import ImageStat
 import torch
+from torchvision.transforms import functional
+from datasets.dataset_retrieval import PlayingCardsDataset
 
 
-def metrics(preds, target):
-    metr = MulticlassF1Score(num_classes=3)
+# https://discuss.pytorch.org/t/computing-the-mean-and-std-of-dataset/34949/13
+class Stats(ImageStat.Stat):
+    def __add__(self, other):
+        return Stats(list(map(add, self.h, other.h)))
 
-    return metr(preds, target)
 
+def calculate_norms(dataset):
+    dl = torch.utils.data.DataLoader(dataset, batch_size=30, num_workers=6)
+
+    stats = None
+    for image, _ in dl:
+        for i in range(image.shape[0]):
+            s = Stats(functional.to_pil_image(image[i]))
+            if stats is None:
+                stats = s
+            else:
+                stats += s
+
+    print(f"mean: {stats.mean}\nstd: {stats.stddev}")
+    # TODO: Write this to a file and implement functionality for reading from file
+
+
+if __name__ == "__main__":
+    calculate_norms(PlayingCardsDataset())
+
+# mean: [198.535528698164, 186.41360832482462, 179.76456663467994]
+# std: [78.065716597632, 85.66597062664934, 87.28034113077337]    
